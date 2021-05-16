26ada2cca099dd127a99f960b123a8c1423b0d81
LJlkdskdjflsa <liliangjya@gmail.com>
2021-05-16 15:42:13 +0800

    connect to oline mongo db

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from env import mongodb_password
+from pymongo import MongoClient
 
-cluster = "mongodb+srv://test:<password>@cluster0.5u3na.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+cluster = "mongodb+srv://test:"+mongodb_password+"@cluster0.5u3na.mongodb.net/test?retryWrites=true&w=majority"
+
+client = MongoClient(cluster)
+
+print(client.list_database_names())
+
+db = client.test
+print(db.list_collection_names())
