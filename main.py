4fc84861b4094280c4e2131740d1d31ab008de53
LJlkdskdjflsa <liliangjya@gmail.com>
2021-05-16 15:52:03 +0800

    insert one

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import datetime
+
 from env import mongodb_password
 from pymongo import MongoClient
 
@@ -6,7 +8,24 @@ cluster = "mongodb+srv://test:"+mongodb_password+"@cluster0.5u3na.mongodb.net/te
 
 client = MongoClient(cluster)
 
-print(client.list_database_names())
+#print(client.list_database_names())
 
 db = client.test
-print(db.list_collection_names())
+#print(db.list_collection_names())
+
+todo1 = {
+  "_id": 1,
+  "name": "lj",
+  "text": "my first todo",
+  "status": "open",
+  "tags": [
+    "python",
+    "coding"
+  ],
+  "date": datetime.datetime.utcnow()
+}
+
+todos = db.todos
+#result = todos.insert_one(todo1)
+
+
